d8ed131a2e02fcba59f8344f430a633346ea57be
전준연 <166536663+junjuny0227@users.noreply.github.com>
2024-09-01 14:04:27 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,8 @@
+function solution(price) {
+    let answer = 0;
+    if (price >= 500000) answer = Math.floor(price -= price / 5);
+    else if (price >= 300000) answer = Math.floor(price -= price / 10);
+    else if (price >= 100000) answer = Math.floor(price -= price / 20);
+    else answer = price;
+    return answer;
+}
