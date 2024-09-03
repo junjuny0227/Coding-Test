b0e9459bac820cea6e0a3652330553258e95fde2
전준연 <166536663+junjuny0227@users.noreply.github.com>
2024-09-03 13:17:14 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -0,0 +1,5 @@
+function solution(my_string) {
+    let answer = 0;
+    for (let i = 0; i < my_string.length; i++) if (!isNaN(my_string[i])) answer += parseInt(my_string[i]); 
+    return answer;
+}
