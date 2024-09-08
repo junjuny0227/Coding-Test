071c9a8286250a61018cf5640be9d59d95071c1f
전준연 <166536663+junjuny0227@users.noreply.github.com>
2024-09-08 15:32:10 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,6 @@
+function solution(n) {
+    let answer = 0;
+    if (n % 2 !== 0) for (let i = n; i > 0; i -= 2) answer += i;
+    else for (let i = n; i > 0; i -= 2) answer += i**2;
+    return answer;
+}
