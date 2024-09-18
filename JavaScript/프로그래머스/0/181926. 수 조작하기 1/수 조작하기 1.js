c6cc6b4915575e3c5fa4c37c0a084f6be7d27350
전준연 <166536663+junjuny0227@users.noreply.github.com>
2024-09-19 08:13:21 +0900

    [level 0] Title: 수 조작하기 1, Time: 7.95 ms, Memory: 38.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
+++ JavaScript/프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,4 +1,5 @@
 function solution(n, control) {
+    console.log(n)
     const arr = control.split("");
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === "w") n += 1;
@@ -6,6 +7,5 @@ function solution(n, control) {
         else if (arr[i] === "d") n += 10;
         else if (arr[i] === "a") n -= 10;
     }
-    let answer = n;
-    return answer;
+    return n;
 }
