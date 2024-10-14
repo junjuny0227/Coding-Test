57743e0a60c6846665450ea20f38e7f44d73466f
전준연 <166536663+junjuny0227@users.noreply.github.com>
2024-10-14 14:44:21 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
+++ JavaScript/프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -1,11 +1,5 @@
 function solution(my_string) {
-    let answer = [];
-    let trim_str = my_string.trim();
-    let i = 0;
-    while (true) {
-        if (trim_str[i] !== " ") answer[i] += trim_str[i];
-        if (trim_str[i + 1] === " ") i++;
-        if (i === trim_str.length - 1) break;
-    }
+    my_string = my_string.split(" ");
+    let answer = my_string.filter((my_string) => my_string !== "");
     return answer;
 }
