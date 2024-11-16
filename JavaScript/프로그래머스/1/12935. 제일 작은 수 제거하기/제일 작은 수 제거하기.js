04ab0093e170c3f29a45be99d0a25ca9231fad3b
전준연 <166536663+junjuny0227@users.noreply.github.com>
2024-11-16 19:38:22 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 4.73 ms, Memory: 43.6 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,7 @@
+function solution(arr) {
+    let answer = [];
+    let minNum = Math.min(...arr);
+    for (let i = 0; i < arr.length; i++) if (arr[i] !== minNum) answer.push(arr[i]);
+    if (answer.length === 0) answer.push(-1);
+    return answer;
+}
