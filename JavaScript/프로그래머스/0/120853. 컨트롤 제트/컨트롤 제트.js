77a091155315813f8dc9f0d41bb8df0e1ae4f428
전준연 <166536663+junjuny0227@users.noreply.github.com>
2025-02-01 19:44:06 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    let answer = 0;
+    const arr = s.split(" ");
+    for (let i = 0; i < arr.length; i++) {
+        if (isNaN(arr[i])) answer -= arr[i - 1];
+        else answer += Number(arr[i]);
+    }
+    return answer;
+}
