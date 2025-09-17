5dbcc2a4873739c85806a8c51b8439de6b487221
전준연 <junjuny0227@gmail.com>
2025-09-18 00:41:16 +0900

    [Bronze V] Title: A+B - 5, Time: 92 ms, Memory: 9356 KB -BaekjoonHub

---
+++ node.js/백준/Bronze/10952. A＋B － 5/A＋B － 5.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').toString().trim().split('\n');
+
+const results = [];
+
+for (let i = 0; i < input.length - 1; i++) {
+  const [a, b] = input[i].split(' ').map(Number);
+  results.push(a + b);
+}
+
+console.log(results.join('\n'));
