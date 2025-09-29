db1875419a46c1929d8dd019d6fdd444667f3574
전준연 <junjuny0227@gmail.com>
2025-09-29 23:02:55 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
@@ -0,0 +1,8 @@
+function solution(myString, pat) {
+    let answer = 0;
+
+    for (let i = 0; i < myString.length; i++)
+        if (myString.slice(i, pat.length + i) === pat) answer++;
+
+    return answer;
+}
