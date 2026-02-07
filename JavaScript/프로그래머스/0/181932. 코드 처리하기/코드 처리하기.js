511ae26e16a2b312a96283e1408f21dd50629014
전준연 <junjuny0227@gmail.com>
2026-02-07 16:45:46 +0900

    [level 0] Title: 코드 처리하기, Time: 9.27 ms, Memory: 39.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,16 @@
+function solution(code) {
+    let ret = '';
+    let mode = 0;
+    
+    for (let idx = 0; idx < code.length; idx++) {
+        if (mode === 0) {
+            if (code[idx] !== '1') idx % 2 === 0 && (ret += code[idx]);
+            else mode = 1;
+        } else {
+            if (code[idx] !== '1') idx % 2 === 1 && (ret += code[idx])
+            else mode = 0;
+        }
+    }
+    
+    return ret.length === 0 ? "EMPTY" : ret;
+}
