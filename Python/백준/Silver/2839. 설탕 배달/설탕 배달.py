0e5900d6b6f15aa81fdfa9fdc157f14cc9a5619e
전준연 <junjuny0227@gmail.com>
2026-03-26 13:45:31 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,18 @@
+def solve():
+    n = int(input())
+
+    count = 0
+    while n > 0:
+        if n % 5 == 0:
+            count += n // 5
+            break
+        n -= 3
+        count += 1
+
+    if n < 0:
+        print(-1)
+    else:
+        print(count)
+
+if __name__ == "__main__":
+    solve()
